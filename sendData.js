2597e547cd1215217133d5e693c03b52397ef86f
Ugur Tigu <ugurtigu@gmail.com>
2020-11-30 09:25:25 +0100

    makes http post request

--- sendData.js
+++ sendData.js
@@ -1,20 +1,23 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 const url = 'https://hidden-ridge-18950.herokuapp.com/api/blogposts';
 const data = {
-  title: 'new',
+  title: 'newest old boys',
   date: '11-12-2020',
   tags: ['text1', 'text2', 'text3'],
   postCategory: 'somecat',
   postContent:
     "<TextBlock>\r\n          But they're barely alone in this - and I can't really blame them\r\n          either. I have guaranteed made similar mistakes in the past, as most\r\n          D3.js tutorials out there don't mention accessibility, and a lot of\r\n          visualization libraries built upon D3.js are inaccessible by default.\r\n        </TextBlock>",
 };
-const otherParams = {
-  headers: { 'content-type': 'application/json; charset=UTF-8' },
-  body: data,
-  method: 'POST',
-};
 
-fetch(url, otherParams)
-  .then((data) => data.json)
-  .then((res) => console.log(res.body))
-  .catch((error) => console.log(error));
+axios({
+  method: 'post',
+  url: url,
+  data: data,
+}).then(
+  (response) => {
+    console.log(response);
+  },
+  (error) => {
+    console.log(error);
+  }
+);
